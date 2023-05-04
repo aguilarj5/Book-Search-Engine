083b02249643ae354b5c6207734c1e091ea34c48
Jags <jon.kmjt@gmail.com>
2023-05-03 21:08:35 -0400

    typedefs updated

--- Develop/server/schemas/typeDefs.js
+++ Develop/server/schemas/typeDefs.js
@@ -1,16 +1,47 @@
 const { gql } = require('apollo-server-express');
 
 const typeDefs = gql`
-  type Class {
-    _id: ID
-    name: String
-    building: String
-    creditHours: Int
+	type Query {
+		me: User
+	}
+
+  input saveBookInput {
+    books: [Book.authors]
+    description: String!
+    title: String!
+    bookId: ID!
+    image: String!
+    link: String!
   }
 
-  type Query {
-    classes: [Class]
-  }
+	type Mutation {
+		login(email: String!, password: String!): Auth
+		addUser(username: String!, email: String!, password: String!): Auth
+    saveBook(criteria: saveBookInput): User
+    removeBook(bookId: String!): User
+	}
+
+	type User {
+		_id: ID
+		username: String
+		email: String
+		bookCount: Number
+		savedBooks: [Book]!
+	}
+
+	type Book {
+		bookId: ID
+		authors: [String]
+		description: String
+		title: String
+		image: String
+		link: String
+	}
+
+	type Auth {
+		token: ID!
+		user: User
+	}
 `;
 
 module.exports = typeDefs;
